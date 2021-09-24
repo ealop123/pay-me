3a69b4f6cce08e499d3606e383e7be8b24cc14cd
Ezekiel Lopez <EzekielAdamLopez@gmail.com>
2021-09-24 11:36:18 -0700

    launch test added

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 ;(() => {
   const $ = str => [...document.querySelectorAll(str)];
-  console.log($("#leftSide")[0])
+  console.log(`
+    ${"-".repeat(10)}launch test${"-".repeat(10)}
+    Checking status of #leftSide load
+
+    ${
+      $("#leftSide")[0] == undefined ?
+      "STATUS DEFER FAILED" :
+      "STATUS GOOD"
+    }
+    ${"-".repeat(10)}end test${"-".repeat(10)}
+
+    `); //launch test
 })();
