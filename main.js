ee8534d54925e5686bdc54935d3d638c60031400
ealop123 <engineeringgeek3.14@gmail.com>
2021-09-30 20:46:59 -0700

    begin js

--- main.js
+++ main.js
@@ -12,4 +12,24 @@
     ${"-".repeat(10)}end test${"-".repeat(10)}
 
     `); //launch test
+
+  /*
+  <div class="messageContext">
+   <div class="messageDescription">
+     <div class="messageProfile panda"></div>
+     <div class="messageInfo">
+       <div class="messageName">Jinx123</div>
+       donated
+       <div class="contributionAmount">$20</div>
+     </div>
+   </div>
+   <div class="message">
+     <p>Hey 👋 I just created a page here.</p>
+   </div>
+ </div>
+  */
+
+  function createMessage({name, amt}) {
+
+  }
 })();
