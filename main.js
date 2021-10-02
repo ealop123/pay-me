d27a7e3b56e5aa4f0554828c8f2e3e758ff38215
ealop123 <engineeringgeek3.14@gmail.com>
2021-10-01 17:40:18 -0700

    redid js

--- main.js
+++ main.js
@@ -1,35 +1,19 @@
-;(() => {
-  const $ = str => [...document.querySelectorAll(str)];
-  console.log(`
-    ${"-".repeat(10)}launch test${"-".repeat(10)}
-    Checking status of #leftSide load
+const main = function () {
 
-    ${
-      $("#leftSide")[0] == undefined ?
-      "STATUS DEFER FAILED" :
-      "STATUS GOOD"
-    }
-    ${"-".repeat(10)}end test${"-".repeat(10)}
-
-    `); //launch test
-
-  /*
-  <div class="messageContext">
-   <div class="messageDescription">
-     <div class="messageProfile panda"></div>
-     <div class="messageInfo">
-       <div class="messageName">Jinx123</div>
-       donated
-       <div class="contributionAmount">$20</div>
-     </div>
-   </div>
-   <div class="message">
-     <p>Hey 👋 I just created a page here.</p>
+}
+/*
+<div class="messageContext">
+ <div class="messageDescription">
+   <div class="messageProfile panda"></div>
+   <div class="messageInfo">
+     <div class="messageName">Jinx123</div>
+     donated
+     <div class="contributionAmount">$20</div>
    </div>
  </div>
-  */
-
-  function createMessage({name, amt}) {
-
-  }
-})();
+ <div class="message">
+   <p>Hey 👋 I just created a page here.</p>
+ </div>
+</div>
+*/
+window.addEventListener("load", main);
