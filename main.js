fca994a83752d33a4d1a59549f5628f7d7325f57
teknicolor <tablesa1t68429@gmail.com>
2021-10-01 21:34:05 -0400

    added javascript for message area

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
 const main = function () {
-
+createMessage ();       
 }
+
+function createMessage () {
+  const messageContext = document.createElement('div');
+  messageContext.classList.add('messageContext');
+  document.querySelector('#messageArea')
+    .appendChild(messageContext);
+
+  const messageDescription = document.createElement('div');
+  messageDescription.classList.add('messageDescription');
+  messageContext.appendChild(messageDescription);
+
+  const messageProfile = document.createElement('div');
+  messageProfile.classList.add('messageProfile');
+  messageProfile.classList.add('panda');
+  messageDescription.appendChild(messageProfile);
+
+  const messageInfo = document.createElement('div');
+  messageInfo.classList.add('messageInfo');
+  messageDescription.appendChild(messageInfo);
+
+  const messageName = document.createElement('div');
+  messageName.classList.add('messageName');
+  messageName.innerText = 'Jinx123';
+  messageInfo.appendChild(messageName);
+
+  const textNode = document.createTextNode(' donated ');
+  messageInfo.appendChild(textNode);
+
+  const contributionAmount = document.createElement('div');
+  contributionAmount.classList.add('contributionAmount');
+  contributionAmount.innerText = '$20';
+  messageInfo.appendChild(contributionAmount);
+
+  const message = document.createElement('div');
+  message.classList.add('message');
+  messageContext.appendChild(message);
+
+  const p = document.createElement('p');
+  p.innerText = 'Hey 👋 I just created a page here.';
+  message.appendChild(p);
+}
+
+
 /*
 <div class="messageContext">
  <div class="messageDescription">
